6fd6a24bff55ba88ec1e0b535fc088cba2fa65e0
haniful360 <haniful360@gmail.com>
2023-04-11 00:56:36 +0600

    footer design added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -7,46 +7,48 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className='bg-black mt-24'>
-            <div className='w-[1280px] mx-auto'>
-            <div className='grid grid-cols-1 sm-grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 '>
-                <div>
-                    <h3 className='logo'>Walks of Life</h3>
+            <div className='w-[1280px] mx-auto py-[120px]'>
+            <div className='grid grid-cols-1 sm-grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8  text-white pb-16'>
+                <div className='walks-life'>
+                    <h3 className=''>Walks of Life</h3>
                     <p>There are many variations of passages of Lorem Ipsum , but the majority have suffered alteration in some form.</p>
-                    <div className='social-icons'>
+                    <div className='flex items-center gap-4'>
                         <div className='facebook'>
-                            <BsFacebook></BsFacebook>
+                            <BsFacebook className='fb-icon'></BsFacebook>
                         </div>
                         <div className='twitter'>
-                            <BsTwitter></BsTwitter>
+                            <BsTwitter className='twitter-icon'></BsTwitter>
                         </div>
                         <div className='instagram'>
-                            <BsInstagram></BsInstagram>
+                            <BsInstagram className='
+                            '></BsInstagram>
                         </div>
                     </div>
                 </div>
-                <div>
+                <div className='company-info'>
                     <h5>Company</h5>
                     <Link to="">About us</Link><br />
                     <Link to="">Works</Link><br />
                     <Link to="">Latest News</Link><br />
                     <Link to="">Career</Link><br />
                 </div>
-                <div>
+                <div className='product-info'>
                     <h5>Product</h5>
                     <Link to="">Prototype</Link><br />
                     <Link to="">Plan & Pricing</Link><br />
                     <Link to="">Customers</Link><br />
                     <Link to="">Integrations</Link><br />
                 </div>
-                <div>
-                    <h5>Support</h5>
+                <div className='support-info'>
+                    <h5 >Support</h5>
                     <Link to="">Help Desk</Link><br />
                     <Link to="">Sales</Link><br />
                     <Link to="">Become a Partner</Link><br />
                     <Link to="">Developers</Link><br />
                 </div>
             </div>
-            <div className='lg:flex justify-between items-center'>
+            <hr />
+            <div className='lg:flex justify-between items-center copyright'>
                 <p>@2023 CareerHub. All Rights Reserved</p>
                 <p>Powered by Walks of life</p>
             </div>
