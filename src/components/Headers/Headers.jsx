dff29c9c3da4b2adf2f6021d769775799fd07b63
haniful360 <haniful360@gmail.com>
2023-04-11 00:22:45 +0600

    header style added

--- src/components/Headers/Headers.jsx
+++ src/components/Headers/Headers.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './Headers.css'
 
 const Headers = () => {
     return (
@@ -10,23 +11,23 @@ const Headers = () => {
                         <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-                        <ul tabIndex={0} className="menu menu-compact dropdown-content  mt-3 p-2 shadow bg-base-100 rounded-box w-52 list-item">
+                        <ul tabIndex={0} className="menu menu-compact dropdown-content  mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                             <li><Link to='/'>Statistics</Link></li>
                             <li><Link to='/'>Applied Job</Link></li>
                             <li><Link to='/'>BLog</Link></li>
                         </ul>
                     </div>
-                    <a className="btn btn-ghost normal-case text-xl">Walks Of Life</a>
+                    <Link to="/" className="btn btn-ghost normal-case text-xl">Walks Of Life</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1 list-item">
+                    <ul className="menu menu-horizontal px-1">
                         <li><Link to='/'>Statistics</Link></li>
                         <li><Link to='/'>Applied Job</Link></li>
                         <li><Link to='/'>BLog</Link></li>
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <button className="get-started-btn text-[17px] text-white">Start Applying</button>
+                    <button className="get-started-btn mt-0 text-[17px] text-white">Start Applying</button>
                 </div>
             </div>
         </div>
